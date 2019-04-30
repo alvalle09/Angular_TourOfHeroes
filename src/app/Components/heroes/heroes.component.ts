a8fd5882ea2b54e350069679e1fb88a7a741f60b
alvalle09 <phredvalle@gmail.com>
2019-04-29 21:56:50 -0500

    show list of heroes from hard-coded array for now

--- src/app/Components/heroes/heroes.component.ts
+++ src/app/Components/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../../mock-heroes';
+
+
 
 // use '../../ ' to move up 2 levels in dir structure
 import { Hero } from '../../hero';
@@ -9,11 +12,15 @@ import { TestBed } from '@angular/core/testing';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
+
 export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+
+  heroes =  HEROES;
 
   constructor() {}
 
